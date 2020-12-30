bf04d2721aaa1b5e371bc714919d5feffe38c893
DIALLO Souleymane <72746804+souleymane-diallo@users.noreply.github.com>
2020-12-30 11:15:48 +0100

    gestion des touches de saisies

--- keyboard.js
+++ keyboard.js
@@ -3,3 +3,9 @@ const chk = document.getElementById('chk');
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark');
 });
+
+const test = document.getElementById('.test');
+
+test.addEventListener('keypress', (e) => {
+    console.log(e);
+});
