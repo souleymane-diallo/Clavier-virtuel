38fab0602289d3c2cbbd1ab37b7582e378d6e199
DIALLO Souleymane <72746804+souleymane-diallo@users.noreply.github.com>
2021-02-02 10:30:14 +0100

    ajout de fonction azerty qwerty

--- keyboard.js
+++ keyboard.js
@@ -1,16 +1,14 @@
-const chk = document.getElementById('chk');
-
-chk.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
-});
-
 window.addEventListener("load", function() {
-    const buttons = document.querySelectorAll(".key");
-    const textOuput = document.querySelector(".ecran1");
-    const capsLock = document.querySelector('.capsled');
-    const capsLedOn = document.querySelector('.capsled-on');
-    const shift = document.querySelector('.shift');
-    let isCpasLock = false;
+    const darkSwitch = document.querySelector("#darkSwitch");
+    const keyboardSwitch = document.querySelector("#keyboardSwitch");
+    const cssLink = document.querySelector("#link");
+    const buttons = document.querySelectorAll("input[type=button]");
+    const textOutput = document.querySelector("#text-output");
+    const capsLock = document.querySelector("#capsLock");
+    const capsCircle = document.querySelector(".capsCircle");
+    const shift = document.querySelector("#shift");
+    const shiftCircle = document.querySelector(".shiftCircle");
+    let isCapsLock = false;
     let isShift = false;
     let isAzerty = true;
 
@@ -18,51 +16,98 @@ window.addEventListener("load", function() {
 
     const majAzerty = ["²", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "°", "+", "Delete", "Tab", "A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P", "¨", "£", "Q", "S", "D", "F", "G", "H", "J", "K", "L", "M", "%", "µ", "Enter", "<", "W", "X", "C", "V", "B", "N", "?", ".", "/", "§"];
 
+    const minQwerty = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Delete", "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/"];
 
-    // caps Lock
+    const majQwerty = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "Delete", "Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "|", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", '"', "Enter", "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?"]
+
+    // Dark Mode 
+    cssLink.href = "css/keyboard-dark.css";
+    darkSwitch.addEventListener("click", function() {
+        this.checked ? cssLink.href = "css/keyboard-dark.css" : cssLink.href = "css/keyboard-light.css";
+    });
+
+    // Qwerty / Azerty
+    keyboardSwitch.addEventListener("click", function() {
+        if (this.checked === true) {
+            isAzerty = true;
+            for (let i = 0; i < minAzerty.length; i++) {
+                buttons[i].value = minAzerty[i];
+            }
+        } else {
+            isAzerty = false;
+            for (let i = 0; i < minQwerty.length; i++) {
+                buttons[i].value = minQwerty[i];
+            }
+        }
+    })
+
+    // Caps Lock
     capsLock.addEventListener("click", function() {
-        isCpasLock = !isCpasLock;
-        capsLocked(isCpasLock, capsLedOn);
+        isCapsLock = !isCapsLock;
+        capsLocked(isCapsLock, capsCircle);
     });
 
     // Shift
-
     shift.addEventListener("click", function() {
         isShift = !isShift;
-        capsLocked(isShift, shiftCircle)
-    });
+        capsLocked(isShift, shiftCircle);
+    })
 
+    function capsLocked(boolean, circle) {
+        if (boolean === true) {
+            circle.style.backgroundColor = "green";
 
+            if (isAzerty === true) {
+                for (let i = 0; i < majAzerty.length; i++) {
+                    buttons[i].value = majAzerty[i];
+                }
+            } else {
+                for (let i = 0; i < majQwerty.length; i++) {
+                    buttons[i].value = majQwerty[i];
+                }
+            }
+        } else {
+            circle.style.backgroundColor = "red";
 
-    // Onclick
+            if (isCapsLock === false) {
+                if (isAzerty === true) {
+                    for (let i = 0; i < minAzerty.length; i++) {
+                        buttons[i].value = minAzerty[i];
+                    }
+                } else {
+                    for (let i = 0; i < minQwerty.length; i++) {
+                        buttons[i].value = minQwerty[i];
+                    }
+                }
+            }
+        }
+    }
 
+    // onclick
     buttons.forEach(button =>
         button.addEventListener("click", function() {
             switch (this.value) {
                 case "SPACE":
-                    textOuput.value += " ";
+                    textOutput.value += " ";
                     break;
                 case "Delete":
-                    textOuput.value = textOuput.value.substring(0, textOuput.value.length - 1);
+                    textOutput.value = textOutput.value.substring(0, textOutput.value.length - 1);
                     break;
                 case "Enter":
-                    textOuput.value += "\n";
+                    textOutput.value += "\n";
                     break;
                 case "Tab":
-                    textOuput.value += "  ";
+                    textOutput.value += "   "
                     break;
                 default:
                     if (isShift === true) {
                         isShift = !isShift;
-                        textOuput.value += this.value;
+                        textOutput.value += this.value;
                         capsLocked(isShift, shiftCircle)
                     } else {
-                        textOuput.value += this.value;
+                        textOutput.value += this.value;
                     }
             }
-
-        }))
-
-
-
-})
+        })
+    )
+});
